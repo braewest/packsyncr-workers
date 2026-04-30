a774aa15ef18221ca514406c0d3335d48e61ed39
Braeden West <73409023+braewest@users.noreply.github.com>
2026-04-29 19:38:40 -0700

    Added deletion CORS permission

--- workers/pack-handler/pack-handler.js
+++ workers/pack-handler/pack-handler.js
@@ -31,7 +31,7 @@ const CORS_HEADERS = {
   "Access-Control-Allow-Origin": FRONTEND_ORIGIN,
   "Access-Control-Allow-Credentials": "true",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE",
   "Content-Type": "application/json"
 };
 
